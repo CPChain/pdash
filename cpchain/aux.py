fe756d1c85e44cb4065c9b6f22a527ab25022e93
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-03-20 13:55:02 +0800

    minor

--- cpchain/aux.py
+++ cpchain/aux.py
@@ -1,8 +1,6 @@
 import os.path as osp
+import toml
 
 root_dir = osp.abspath(osp.join(osp.dirname(osp.abspath(__file__)), '../'))
-
-
-import toml
 
 config = toml.load(osp.join(root_dir, 'cpchain/cpchain.toml'))
