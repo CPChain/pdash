96e982f6e4ef9b2724848b81b1f1b62f9df73dd9
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-03-21 11:42:19 +0800

    update configuration facility

--- cpchain/aux.py
+++ cpchain/aux.py
@@ -1,8 +1,19 @@
 import os.path as osp
+import toml
 
 root_dir = osp.abspath(osp.join(osp.dirname(osp.abspath(__file__)), '../'))
 
 
-import toml
+class Config:
+    def __init__(self, conf):
+        self.conf = conf
 
-config = toml.load(osp.join(root_dir, 'cpchain/cpchain.toml'))
+    def __getattr__(self, name):
+        if not isinstance(self.conf[name], dict):
+            return self.conf[name]
+        return Config(self.conf[name])
+
+    def __getitem__(self, key):
+        return self.conf[key]
+
+config = Config(toml.load(osp.join(root_dir, 'cpchain/cpchain.toml')))
