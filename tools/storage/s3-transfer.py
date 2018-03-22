e66e83bae720c622b089a558a2b9a609a3ee7b53
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-03-22 15:23:59 +0800

    add aes-cfb streaming cypher support

--- tools/storage/s3-transfer.py
+++ tools/storage/s3-transfer.py
@@ -5,6 +5,6 @@ from cpchain.storage import S3Storage
 
 storage = S3Storage()
 
-storage.upload_file(osp.join(cpchain.root_dir, "tools/storage/shakespeare.txt"), "sp")
+storage.upload_file(osp.join(cpchain.root_dir, "tools/assets/shakespeare.txt"), "sp")
 
 storage.download_file(osp.join(cpchain.root_dir, "tools/storage/_local_shakespeare.txt"), "sp", fsize=5300)
