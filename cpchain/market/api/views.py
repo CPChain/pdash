b97a59263b2cac39750cede89d2ac9e5fe87752c
xumx <xumx@cpchain.io>
2018-03-28 13:46:44 +0800

    support product publish

--- cpchain/market/api/views.py
+++ cpchain/market/api/views.py
@@ -13,37 +13,44 @@ from .permissions import IsOwnerOrReadOnly
 from .serializers import *
 
 
-# login
+PUBLIC_KEY = "public_key"
+
+
 class UserLoginAPIView(APIView):
+    """
+    API endpoint that used to login.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
     def post(self, request):
         data = request.data
-        public_key = data.get('username')
+        public_key = data.get(PUBLIC_KEY)
         password = data.get('password')
-        user = User.objects.get(username__exact=public_key)
+        user = User.objects.get(public_key__exact= public_key)
         if user.password == password:
             serializer = UserSerializer(user)
             new_data = serializer.data
             # set userid ins session
             self.request.session['user_id'] = user.id
-            return Response(new_data, status=HTTP_200_OK)
+            return Response(new_data, status= HTTP_200_OK)
         return Response('password error', HTTP_400_BAD_REQUEST)
 
 
-# 用于注册
 class UserRegisterAPIView(APIView):
+    """
+    API endpoint that used to register user account.
+    """
     queryset = User.objects.all()
     serializer_class = UserRegisterSerializer
     permission_classes = (AllowAny,)
 
     def post(self, request):
         data = request.data
-        public_key = data.get("public_key")
+        public_key = data.get(PUBLIC_KEY)
         if User.objects.filter(public_key__exact=public_key):
-            return Response("user name already exists!",HTTP_400_BAD_REQUEST)
+            return Response("public_key already exists!", HTTP_400_BAD_REQUEST)
         serializer = UserRegisterSerializer(data=data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
@@ -53,28 +60,44 @@ class UserRegisterAPIView(APIView):
                 'message': 'success',
             }
             return JsonResponse(d)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status= HTTP_400_BAD_REQUEST)
 
 
 class LogoutAPIView(APIView):
+    """
+    API endpoint that logout.
+    """
     def post(self):
         if self.request.session['user_id'] is not None:
             self.request.session['user_id'] = None
-            return Response({"message":"登出成功"}, status=HTTP_200_OK)
+            return Response({"message": "logout success"}, status= HTTP_200_OK)
         else:
-            return Response({"message":"暂未登陆，无需登出"}, status=HTTP_400_BAD_REQUEST)
+            return Response({"message": "not login"}, status= HTTP_400_BAD_REQUEST)
 
 
-# query products
 class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows query products.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (AllowAny,)
+    # permission_classes = (IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
-        serializer.save(owner=User.objects.get(id=self.request.session.get('user_id')))
+        # user_id = self.request.session.get('user_id')
+        # print('userId:' + user_id)
+        # if user_id is None:
+        #     print("userId is none")
+        #     user_id = 1
+        #     print('user_id:', user_id)
+
+        serializer.save(owner=User.objects.get(id=1))
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Products to be viewed by anyone.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
--- cpchain/market/market/urls.py
+++ cpchain/market/market/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls_v1')),
 
 ]
