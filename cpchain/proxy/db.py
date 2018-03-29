511437ea21e4462ec9106bb13014c693376a1aeb
Borun Fu <fujh@cpchain.io>
2018-03-29 19:22:15 +0800

    add SQLite database support

---
+++ cpchain/proxy/db.py
@@ -0,0 +1,75 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer
+from sqlalchemy.dialects.mysql import BINARY, TIMESTAMP
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from datetime import datetime, timedelta
+
+Base = declarative_base()
+
+class Trade(Base):
+    __tablename__ = 'Trade'
+
+    id = Column(Integer, primary_key=True)
+    buyer_addr = Column(BINARY, nullable=False)
+    seller_addr = Column(BINARY, nullable=False)
+    market_hash = Column(BINARY, nullable=False)
+    AES_key = Column(BINARY, nullable=False)
+    file_hash = Column(BINARY, nullable=False)
+    time_stamp = Column(TIMESTAMP, nullable=False)
+
+    def __repr__(self):
+        return "<Trade(buyer_addr='%s', seller_addr='%s', \
+            market_hash='%s', AES_key='%s', file_hash='%s' \
+            time_stamp='%s')>" % (self.buyer_addr, \
+            self.seller_addr, self.market_hash, self.AES_key, \
+            self.file_hash, self.time_stamp)
+
+def db_session_create(db='sqlite:///./trade.db'):
+    engine = create_engine(db, echo=False)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+def db_count(session, trade):
+    count = session.query(Trade).filter(\
+                Trade.buyer_addr ==  trade.buyer_addr,
+                Trade.seller_addr == trade.seller_addr, \
+                Trade.market_hash == trade.market_hash, \
+                Trade.AES_key == trade.AES_key, \
+                Trade.file_hash == trade.file_hash).count()
+    return count
+
+def db_query(session, trade):
+    instances = session.query(Trade).filter(\
+                Trade.buyer_addr ==  trade.buyer_addr,
+                Trade.seller_addr == trade.seller_addr, \
+                Trade.market_hash == trade.market_hash, \
+                Trade.AES_key == trade.AES_key, \
+                Trade.file_hash == trade.file_hash).all()
+    return instances
+
+def db_insert(session, trade):
+    trade.time_stamp = datetime.now()
+    session.add(trade)
+    session.commit()
+
+def db_delete(session, trade):
+    session.delete(trade)
+    session.commit()
+
+def db_session_close(session):
+    session.close()
+
+def db_reclaim(db='sqlite:///./trade.db'):
+    session = db_session_create(db)
+
+    reclaim_period = datetime.now() - timedelta(days=3)
+    for instance in session.query(Trade).filter(\
+                    Trade.time_stamp < reclaim_period).all():
+        db_delete(session, instance)
+
+    session.commit(session)
+    db_session_close(session)
