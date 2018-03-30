ed6840e3bd835034c4617b32644ea8228ea7a6ee
xumx <xumx@cpchain.io>
2018-03-30 13:24:39 +0800

    ordering WalletUser,add Token support

--- cpchain/market/api/models.py
+++ cpchain/market/api/models.py
@@ -1,10 +1,10 @@
-import datetime
-
+import os
+import binascii
 from django.contrib.auth.models import (
     AbstractBaseUser
 )
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 
 class WalletUser(models.Model):
     public_key = models.CharField(max_length=200, unique=True)
@@ -14,6 +14,9 @@ class WalletUser(models.Model):
 
     USERNAME_FIELD = 'public_key'
     REQUIRED_FIELDS = ['public_key']
+
+    class Meta:
+        ordering = ('created', 'public_key')
 
     def __str__(self):
         return self.public_key
@@ -28,3 +31,36 @@ class Product(models.Model):
     created = models.DateTimeField('Created', auto_now_add=True)
     expired_date = models.DateTimeField('date expired', null=True)
     verify_code = models.CharField(max_length=200, null=True)
+
+
+class Token(models.Model):
+    """
+    The default authorization token model.
+    """
+    key = models.CharField(_("Key"), max_length=40, primary_key=True)
+    user = models.OneToOneField(
+        WalletUser, related_name='auth_token',
+        on_delete=models.CASCADE, verbose_name=_("WalletUser")
+    )
+    created = models.DateTimeField(_("Created"), auto_now_add=True)
+
+    class Meta:
+        # Work around for a bug in Django:
+        # https://code.djangoproject.com/ticket/19422
+        #
+        # Also see corresponding ticket:
+        # https://github.com/encode/django-rest-framework/issues/705
+        # abstract = 'rest_framework.authtoken' not in settings.INSTALLED_APPS
+        verbose_name = _("Token")
+        verbose_name_plural = _("Tokens")
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(Token, self).save(*args, **kwargs)
+
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def __str__(self):
+        return self.key
