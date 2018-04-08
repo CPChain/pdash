b0087fda90c0dba6c89d0abc84b655409788cfc8
HuangRenfei <renfei@HuangRenfeis-MacBook-Pro.local>
2018-04-08 14:07:02 +0800

    minor code changes

--- cpchain/wallet/fs.py
+++ cpchain/wallet/fs.py
@@ -12,42 +12,31 @@ def get_file_list():
 def add_file(new_file):
     session.add(new_file)
     session.commit()
-    return
 
 
 def delete_file(file_name):
     session.query(FileInfo).filter(FileInfo.name == file_name).\
         delete(synchronize_session=False)
     session.commit()
-    return
 
 
 def encrypt_file(key, file_in_path, file_out_path):
     encrypter = AESCipher(key=key)
     encrypter.encrypt(file_in_path, file_out_path)
-    return
 
 
 def decrypt_file(key, file_in_path, file_out_path):
     decrypter = AESCipher(key=key)
     decrypter.decrypt(file_in_path, file_out_path)
-    return
 
 
-def upload_file(file_path, storage_type="IPFS"):
-    if storage_type == "IPFS":
-        storage = IPFSStorage()
-    else:
-        storage = S3Storage()
+def upload_file_ipfs(file_path):
+    storage = IPFSStorage()
     storage.upload_file(file_path)
-    return
 
 
-def download_file(fhash, file_path, storage_type="IPFS"):
-    if storage_type == "IPFS":
-        storage = IPFSStorage()
-    else:
-        storage = S3Storage()
-    storage.download_file(fhash=fhash, fpath=file_path)
-    return
+def download_file_ipfs(fhash, file_path):
+    storage = IPFSStorage()
+    return storage.download_file(fhash=fhash, fpath=file_path)
+
 
