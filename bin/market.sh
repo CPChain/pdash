916353b4b7333ee69eee918ff7f7d71050299e0c
xumx <xumx@cpchain.io>
2018-04-09 16:24:38 +0800

    add market startup script

--- bin/market.sh
+++ bin/market.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-cd ..
-export PythonPath=`pwd`
 
-python cpchain/market/manage.py runserver
+curr_dir=`pwd`
+cd ..
+ROOT_PATH=`pwd`
+export PYTHONPATH=$PYTHONPATH:$ROOT_PATH
+nohup python cpchain/market/manage.py runserver 0.0.0.0:8000 &
+cd $curr_dir
