4f69e01bd9daaf15eec13588ac355dbc78741492
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-04-12 16:19:12 +0800

    change to old named_tuple syntax

--- cpchain/chain/models.py
+++ cpchain/chain/models.py
@@ -1,11 +1,16 @@
-from typing import NamedTuple
+import collections
+
+order_fields = ['desc_hash', 'seller', 'proxy', 'secondary_proxy', 'proxy_value', 'value', 'time_allowed']
+OrderInfo = collections.namedtuple('OrderInfo', order_fields)
+
+# from typing import NamedTuple
+# class OrderInfo(NamedTuple):
+#     desc_hash: bytearray
+#     seller: bytearray
+#     proxy: bytearray
+#     secondary_proxy: bytearray
+#     proxy_value: int
+#     value: int
+#     time_allowed: int
 
 
-class OrderInfo(NamedTuple):
-    desc_hash: bytearray
-    seller: bytearray
-    proxy: bytearray
-    secondary_proxy: bytearray
-    proxy_value: int
-    value: int
-    time_allowed: int
