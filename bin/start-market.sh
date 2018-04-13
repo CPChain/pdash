f2dfe5ebfdf0930cfee9d26a2f6e453d131a0453
xumx <xumx@cpchain.io>
2018-04-13 11:13:59 +0800

    change market server port to 8083

--- bin/start-market.sh
+++ bin/start-market.sh
@@ -4,5 +4,5 @@ curr_dir=`pwd`
 cd ..
 ROOT_PATH=`pwd`
 export PYTHONPATH=$PYTHONPATH:$ROOT_PATH
-nohup python cpchain/market/manage.py runserver 0.0.0.0:8000 &
+nohup python cpchain/market/manage.py runserver 0.0.0.0:8083 &
 cd $curr_dir
