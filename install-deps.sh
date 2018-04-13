02a38d55763ba4931919e913f36327582474ef8e
millennium-falcon42 <millennium_falcon42@yahoo.co.jp>
2018-04-13 18:07:54 +0800

    add install deps

---
+++ install-deps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+sudo apt install -y python3-pyqt5
+
+sudo apt install -y python3-twisted python3-treq
+
+sudo apt install -y python3-protobuf
+# compiler is only needed for dev.
+# sudo apt install -y protobuf-compiler python3-protobuf
+
+sudo apt install -y python3-cryptography python3-sqlalchemy python3-toml python3-boto3
+
+pip3 install -Ur requirements-dev.txt
+
