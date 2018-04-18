28d20afa8ba0c52e16c6fb919b7c96092277026d
HuangRenfei <renfei@HuangRenfeis-MacBook-Pro.local>
2018-04-18 13:55:15 +0800

    Fix signing verification of proxy.:

--- cpchain/proxy/client.py
+++ cpchain/proxy/client.py
@@ -147,6 +147,7 @@ if __name__ == '__main__':
     buyer_private_key, buyer_public_key = \
                 ECCipher.generate_key_pair(buyer_private_key)
 
+
     seller_private_key = None
     seller_private_key = b'\xa6\xf8_\xee\x1c\x85\xc5\x95\x8d@\x9e\xfa\x80\x7f\xb6\xe0\xb4u\x12\xb6\xdf\x00\xda4\x98\x8e\xaeR\x89~\xf6\xb5'
     seller_private_key, seller_public_key = \
