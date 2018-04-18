5bc30c37c04de7cf609be655ba43520c593fce79
HuangRenfei <renfei@HuangRenfeis-MacBook-Pro.local>
2018-04-18 16:01:02 +0800

    Change claim_relay in server.py in proxy with real SHA256 hash of order_id to confirm the delivery.

--- cpchain/proxy/server.py
+++ cpchain/proxy/server.py
@@ -6,6 +6,8 @@
 
 import sys, os, time
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 
 from twisted.internet import reactor, threads, protocol, ssl
 from twisted.protocols.basic import NetstringReceiver
@@ -18,13 +20,15 @@ from twisted.web.static import File
 from cpchain import config, root_dir
 from cpchain.proxy.msg.trade_msg_pb2 import Message, SignMessage
 from cpchain.proxy.message import message_sanity_check, sign_message_verify
-from cpchain.crypto import pub_key_der_to_addr
+from cpchain.crypto import pub_key_der_to_addr, ECCipher, Encoder
 
 from cpchain.storage import IPFSStorage
 from cpchain.proxy.proxy_db import Trade, ProxyDB
 
 from cpchain.chain.trans import ProxyTrans
 from cpchain.chain.utils import default_web3
+from cpchain.utils import join_with_root
+from eth_utils import to_bytes
 
 server_root = os.path.join(config.home, config.proxy.server_root)
 server_root = os.path.expanduser(server_root)
@@ -141,8 +145,15 @@ class SSLServerProtocol(NetstringReceiver):
 
     def proxy_claim_relay(self):
         proxy_trans = ProxyTrans(default_web3, config.chain.core_contract)
-        # TODO Should sign order id and sha256 here
-        deliver_hash = b'1' * 32
+        private_key_file_path = join_with_root(config.wallet.private_key_file)
+        password_path = join_with_root(config.wallet.private_key_password_file)
+        with open(password_path) as f:
+            password = f.read()
+        priv_key, pub_key = ECCipher.geth_load_key_pair_from_private_key(private_key_file_path, password)
+        priv_key_bytes = Encoder.str_to_base64_byte(priv_key)
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        digest.update(ECCipher.generate_signature(priv_key_bytes, to_bytes(self.trade.order_id), password))
+        deliver_hash = digest.finalize()
         tx_hash = proxy_trans.claim_relay(self.trade.order_id, deliver_hash)
         return tx_hash
 
