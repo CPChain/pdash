231c4ca41dda9078abd24fb18c481245290f26b6
hyiwr <blockchain@ubuntu-17.10.1>
2018-05-10 14:48:12 +0800

    add sidebar (incomplete)

--- cpchain/wallet/ui.py
+++ cpchain/wallet/ui.py
@@ -41,6 +41,103 @@ def load_stylesheet(wid, name):
         wid.setStyleSheet(s.substitute(subs))
 
 
+class SideBar(QScrollArea):
+    def __init__(self, parent):
+        super().__init__(parent)
+        # needed
+        self.parent = parent
+        self.content_tabs = parent.content_tabs
+
+        self.init_ui()
+
+
+    def init_ui(self):
+        self.setObjectName("sidebar")
+        self.setMaximumWidth(180)
+
+        self.frame = QFrame()
+        self.setWidget(self.frame)
+        self.setWidgetResizable(True)
+        self.frame.setMinimumWidth(150)
+
+        def add_labels():
+            self.trend_label = QLabel("Trending")
+            self.trend_label.setObjectName(trend_label)
+            self.trend_label.setMaximumHeight(25)
+
+            self.mine_label = QLabel("Mine")
+            self.mine_label.setObjectName(mine_label)
+            self.trend_label.setMaximumHeight(25)
+
+            self.treasure_label = QLabel("Treasure")
+            self.treasure_label.setObjectName(treasure_label)
+            self.treasure_label.setMaximumHeight(25)
+        add_labels()
+
+        def add_lists():
+            self.trending_list = QListWidget()
+            self.trending_list.addItem(QListWidgetItem(get_icon("cloud_store.png"), "Popular"))
+            self.trending_list.addItem(QListWidgetItem(get_icon("publish_data.png"), "Following"))
+
+            self.mine_list = QListWidget()
+            self.mine_list.addItem(QListWidgetItem(get_icon("browse_market.png"), "Cloud"))
+            self.mine_list.addItem(QListWidgetItem(get_icon("treasur.png"), "Selling"))
+
+            self.treasure_list = QListWidget()
+            self.treasure_list.addItem(QListWidgetItem(get_icon("browse_market.png"), "Purchased"))
+            self.treasure_list.addItem(QListWidgetItem(get_icon("browse_market.png"), "Collection"))
+            self.treasure_list.addItem(QListWidgetItem(get_icon("browse_market.png"), "Shopping Cart"))
+
+            self.feature_list.setCurrentRow(0)
+        add_lists()
+
+        def bind_slots():
+            def trending_list_clicked(item):
+                item_to_tab_name = {
+                    "Popular": "popular_tab",
+                    "Following": "follow_tab",
+                }
+                wid = self.content_tabs.findChild(QWidget, item_to_tab_name[item.text()])
+                self.content_tabs.setCurrentWidget(wid)
+            self.trending_list.itemPressed.connect(trending_list_clicked)
+
+            def mine_list_clicked(item):
+                item_to_tab_name = {
+                    "Cloud": "cloud_tab",
+                    "Selling": "selling_tab",
+                }
+                wid = self.content_tabs.findChild(QWidget, item_to_tab_name[item.text()])
+                self.content_tabs.setCurrentWidget(wid)
+            self.mine_list.itemPressed.connect(mine_list_clicked)
+
+            def treasure_list_clicked(item):
+                item_to_tab_name = {
+                    "Purchased": "purchase_tab",
+                    "Collection": "collect_tab",
+                    "Shopping Cart": "cart_tab",
+                }
+                wid = self.content_tabs.findChild(QWidget, item_to_tab_name[item.text()])
+                self.content_tabs.setCurrentWidget(wid)
+            self.treasure_list.itemPressed.connect(treasure_list_clicked)
+
+        bind_slots()
+
+        def set_layout():
+            self.main_layout = main_layout = QVBoxLayout(self.frame)
+            main_layout.addSpacing(0)
+            main_layout.addWidget(self.trend_label)
+            main_layout.addWidget(self.trending_list)
+            main_layout.addWidget(self.mine_label)
+            main_layout.addWidget(self.mine_list)
+            main_layout.addWidget(self.treasure_label)
+            main_layout.addWidget(self.treasure_list)
+            main_layout.setContentsMargins(0, 10, 0, 0)
+            self.setLayout(self.main_layout)
+        set_layout()
+
+        print("Loading stylesheet of Sidebar")
+
+
 class Header(QFrame):
     class SearchBar(QLineEdit):
         def __init__(self, parent=None):
@@ -191,10 +288,6 @@ class Header(QFrame):
             self.m_drag = False
 
 
-
-
-
-
 class MainWindow(QMainWindow):
     def __init__(self, reactor):
         super().__init__()
@@ -228,9 +321,7 @@ class MainWindow(QMainWindow):
 
         # add panes
         self.header = Header(self)
-
-        # Temporily modified for easy test by @hyiwr
-        print("Adding sidebar......")
+        self.sidebar = SideBar(self)
 
 
         # set layout
