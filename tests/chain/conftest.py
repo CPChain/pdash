86e4408297ed38f63348009b230e9c7c15736ad0
liuq <liuqian@cpchain.io>
2018-05-29 00:29:29 -0700

    fixed tests in tests.chain.

--- tests/chain/conftest.py
+++ tests/chain/conftest.py
@@ -12,30 +12,36 @@ os.chdir(root_dir)
 
 @pytest.fixture(scope="module")
 def contract_name():
-    new_contract_name = config.chain.core_contract
-    chain.utils.deploy_contract(new_contract_name)
+    w3 = chain.utils.default_w3
+    bin_path = chain.utils.join_with_root(config.chain.contract_bin_path)
+    new_contract_name = config.chain.contract_name
+
+    chain.utils.deploy_contract(bin_path, new_contract_name, w3)
     return new_contract_name
 
 
 @pytest.fixture(scope="module")
 def btrans(contract_name):
-    buyer_web3 = chain.utils.default_web3
-    print('buyer: defaultAccount:'.format(buyer_web3.eth.defaultAccount))
-    trans_obj = chain.agents.BuyerAgent(buyer_web3, contract_name=contract_name)
+    buyer_web3 = chain.utils.default_w3
+    print('buyer: defaultAccount:{}'.format(buyer_web3.eth.defaultAccount))
+    bin_path = chain.utils.join_with_root(config.chain.contract_bin_path)
+    trans_obj = chain.agents.BuyerAgent(buyer_web3, bin_path, contract_name)
     return trans_obj
 
 
 @pytest.fixture()
 def strans(contract_name):
-    seller_web3 = chain.utils.default_web3
-    print('seller: defaultAccount:'.format(seller_web3.eth.defaultAccount))
-    trans_obj = chain.agents.SellerAgent(chain.utils.default_web3, contract_name=contract_name)
+    seller_web3 = chain.utils.default_w3
+    print('seller: defaultAccount:{}'.format(seller_web3.eth.defaultAccount))
+    bin_path = chain.utils.join_with_root(config.chain.contract_bin_path)
+    trans_obj = chain.agents.SellerAgent(seller_web3, bin_path, contract_name)
     return trans_obj
 
 
 @pytest.fixture()
 def ptrans(contract_name):
-    proxy_web3 = chain.utils.default_web3
-    print('seller: defaultAccount:'.format(proxy_web3.eth.defaultAccount))
-    trans_obj = chain.agents.ProxyAgent(chain.utils.default_web3, contract_name=contract_name)
+    proxy_web3 = chain.utils.default_w3
+    print('seller: defaultAccount:{}'.format(proxy_web3.eth.defaultAccount))
+    bin_path = chain.utils.join_with_root(config.chain.contract_bin_path)
+    trans_obj = chain.agents.ProxyAgent(proxy_web3, bin_path, contract_name)
     return trans_obj
--- tests/chain/test_timeout.py
+++ tests/chain/test_timeout.py
@@ -1,6 +1,7 @@
 import time
 
 from cpchain.chain.models import OrderInfo
+from cpchain.crypto import RSACipher
 
 
 test_trans_id = 0
@@ -10,8 +11,10 @@ time_allowed = 20
 def test_place_order(btrans):
     global test_trans_id
 
+    buyer_rsa_pubkey = RSACipher.load_public_key()
     order_info = OrderInfo(
-        desc_hash=bytearray([0, 1, 2, 3] * 8),
+        desc_hash=bytes([0, 1, 2, 3] * 8),
+        buyer_rsa_pubkey=buyer_rsa_pubkey,
         seller=btrans.web3.eth.defaultAccount,
         proxy=btrans.web3.eth.defaultAccount,
         secondary_proxy=btrans.web3.eth.defaultAccount,
@@ -21,27 +24,25 @@ def test_place_order(btrans):
     )
 
     test_trans_id = btrans.place_order(order_info)
-    assert test_trans_id == 0
+    assert test_trans_id == 1
     test_record = btrans.query_order(test_trans_id)
-    assert bytes(test_record[0], encoding="utf8") == bytearray([0, 1, 2, 3] * 8)
+    assert test_record[0] == bytes([0, 1, 2, 3] * 8)
     assert test_record[2] == btrans.web3.eth.defaultAccount
     # Check state is Created
-    assert test_record[9] == 0
+    assert test_record[10] == 0
 
 
 def test_relay_claim(ptrans):
-    ptrans.claim_relay(test_trans_id, bytearray([2, 3, 4, 5] * 8))
+    ptrans.claim_relay(test_trans_id, bytes([2, 3, 4, 5] * 8))
     test_record = ptrans.query_order(test_trans_id)
     # Check state is delivered
-    assert test_record[9] == 1
-
-
-time.sleep(time_allowed + 5)
+    assert test_record[10] == 1
 
 
 def test_seller_claim_timeout(strans):
+    time.sleep(time_allowed + 5)
     strans.claim_timeout(test_trans_id)
     test_record = strans.query_order(test_trans_id)
     # Check time out is emitted and order is finished
-    assert test_record[9] == 3
+    assert test_record[10] == 3
 
