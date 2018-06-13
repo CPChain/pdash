a11eb965fc3994e2ff08dfe5e737ffad00fcbdab
liuq <liuqian@cpchain.io>
2018-06-12 21:36:47 -0700

    fixed chain account bugs and test cases.

--- tests/chain/test_normal_process.py
+++ tests/chain/test_normal_process.py
@@ -1,8 +1,9 @@
 from cpchain.chain.models import OrderInfo
-from cpchain.crypto import RSACipher
+from cpchain.crypto import RSACipher, get_addr_from_public_key
+from cpchain.account import Accounts
 
 test_trans_id = 0
-time_allowed = 60
+time_allowed = 200000
 
 def test_proxy_deposit(ptrans):
 
@@ -16,13 +17,16 @@ def test_proxy_deposit(ptrans):
     assert ptrans.query_deposit() == balance + value - withdraw
 
 def test_place_order(btrans):
+    sellerAccount = get_addr_from_public_key(Accounts()[1].public_key)
+    proxyAccount = get_addr_from_public_key(Accounts()[2].public_key)
+
     buyer_rsa_pubkey = RSACipher.load_public_key()
     order_info = OrderInfo(
         desc_hash=bytes([0, 1, 2, 3] * 8),
         buyer_rsa_pubkey=buyer_rsa_pubkey,
-        seller=btrans.web3.eth.defaultAccount,
-        proxy=btrans.web3.eth.defaultAccount,
-        secondary_proxy=btrans.web3.eth.defaultAccount,
+        seller=btrans.web3.toChecksumAddress(sellerAccount),
+        proxy=btrans.web3.toChecksumAddress(proxyAccount),
+        secondary_proxy=btrans.web3.toChecksumAddress(proxyAccount),
         proxy_value=10,
         value=20,
         time_allowed=time_allowed
@@ -32,7 +36,7 @@ def test_place_order(btrans):
     #assert test_trans_id == 1
     test_record = btrans.query_order(test_trans_id)
     assert test_record[0] == bytes([0, 1, 2, 3] * 8)
-    assert test_record[2] == btrans.web3.eth.defaultAccount
+    assert test_record[2] == btrans.account
     # Check state is Created
     assert test_record[10] == 0
 
@@ -44,6 +48,8 @@ def test_seller_confirm_order(strans):
 
 def test_proxy_fetched(ptrans):
     assert ptrans.check_order_is_ready(test_trans_id)
+    test_record = ptrans.query_order(test_trans_id)
+    assert ptrans.account == test_record[4]
     ptrans.claim_fetched(test_trans_id)
     test_record = ptrans.query_order(test_trans_id)
     assert test_record[10] == 2
