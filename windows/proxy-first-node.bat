8d17d8e6894f0d9bc1115f5717e3c695e0867bee
Borun Fu <fujh@cpchain.io>
2018-07-09 16:27:25 +0800

    cpchain/win-support: add startup scripts
    Former-commit-id: 6bcb19e0a841c74e489b4c4900dd431c72fed181

---
+++ windows/proxy-first-node.bat
@@ -0,0 +1,7 @@
+set PYTHONPATH=%CD%\..\;%CD%\Python\Python36\python36.zip;%CD%\Python\Python36\DLLs;%CD%\Python\Python36\DLLs\lib;%CD%\Python\Python36;%CD%\Python\Python36\lib;%CD%\Python\Python36;%CD%\Python\Python36\lib\site-packages;%CD%\Python\Python36\lib\site-packages\win32;%CD%\Python\Python36\lib\site-packages\win32\lib;%CD%\Python\Python36\lib\site-packages\Pythonwin
+
+set CPCHAIN_HOME_CONFIG_PATH=%CD%\.cpchain\cpchain-proxy.toml
+
+set PATH=%CD%\Python\Python36;%PATH%
+
+python.exe ..\bin\proxy-first-node
