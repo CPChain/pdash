167706161983dc7a97a2016f8d8cdf2a309b56ff
Borun Fu <fujh@cpchain.io>
2018-08-23 11:25:46 +0800

    proxy/testcase: update proxy central net testcase
    Former-commit-id: c58343ecb5ab70d4034c264cfc4b21f1434b431a

--- tests/proxy/test_centralnet.py
+++ tests/proxy/test_centralnet.py
@@ -5,41 +5,31 @@ from twisted.test import proto_helpers
 
 import msgpack
 
-from cpchain.proxy.network import PeerProtocol, generate_tid
-from cpchain.proxy.account import set_proxy_account, get_proxy_id, sign_proxy_data
+from cpchain.proxy.centralnet import PeerProtocol
 
-set_proxy_account()
-peer_id = get_proxy_id()
-service_port = 5678
+peer_id = 'fake_peer_id'
 
 def fake_bootstrap():
-    tid = generate_tid()
     msg = {
         'type': 'bootstrap',
-        'tid': tid,
-        'sign_tid': sign_proxy_data(tid),
-        'peer_ip': None,
+        'tid': 'fake_tid',
         'peer_id': peer_id,
-        'peer_info': service_port,
-        'peer_conf': None
         }
+
+    return msg
+
+def fake_ping():
+    msg = {
+        'type': 'ping',
+        'tid': 'fake_tid',
+    }
 
     return msg
 
 def fake_pick_peer():
     msg = {
         'type': 'pick_peer',
-        'tid': generate_tid(),
-        'sysconf': None
-    }
-
-    return msg
-
-def fake_get_peer():
-    msg = {
-        'type': 'get_peer',
-        'peer_id': peer_id,
-        'tid': generate_tid()
+        'tid': 'fake_tid',
     }
 
     return msg
@@ -61,20 +51,21 @@ class PeerProtocolTest(unittest.TestCase):
         msg = msgpack.unpackb(data, raw=False)
         self.assertEqual(addr, self.peer_addr)
         self.assertEqual(msg['type'], 'response')
-        self.assertEqual(msg['data'], 'bootstrap')
-        self.assertEqual(
-            self.protocol.peers[peer_id]['addr'],
-            self.peer_addr)
+        self.assertEqual(msg['response'], 'ok')
+
+    def test_ping(self):
+        msg = fake_ping()
+        data = msgpack.packb(msg, use_bin_type=True)
+        self.protocol.datagramReceived(data, self.peer_addr)
+        data, addr = self.transport.written[0]
+        msg = msgpack.unpackb(data, raw=False)
+        self.assertEqual(addr, self.peer_addr)
+        self.assertEqual(msg['type'], 'response')
+        self.assertEqual(msg['response'], 'ok')
 
     def test_pick_peer(self):
-        peer = {
-            'addr': self.peer_addr,
-            'peer_info': service_port,
-            'ts': time.time()
-            }
 
-        self.protocol.peers[peer_id] = peer
-        self.protocol.peers_lat[peer_id] = 1
+        self.protocol.peers[peer_id] = self.peer_addr
 
         msg = fake_pick_peer()
         data = msgpack.packb(msg, use_bin_type=True)
@@ -83,23 +74,4 @@ class PeerProtocolTest(unittest.TestCase):
         msg = msgpack.unpackb(data, raw=False)
         self.assertEqual(addr, self.peer_addr)
         self.assertEqual(msg['type'], 'response')
-        self.assertEqual(msg['data'], peer_id)
-
-    def test_get_peer(self):
-        peer = {
-            'addr': self.peer_addr,
-            'peer_info': service_port,
-            'ts': time.time()
-            }
-
-        self.protocol.peers[peer_id] = peer
-
-        msg = fake_get_peer()
-        data = msgpack.packb(msg, use_bin_type=True)
-        self.protocol.datagramReceived(data, self.peer_addr)
-        data, addr = self.transport.written[0]
-        msg = msgpack.unpackb(data, raw=False)
-        self.assertEqual(addr, self.peer_addr)
-        self.assertEqual(msg['type'], 'response')
-        self.assertEqual(msg['data'][0], self.peer_addr[0])
-        self.assertEqual(msg['data'][1], service_port)
+        self.assertEqual(msg['response'], peer_id)
