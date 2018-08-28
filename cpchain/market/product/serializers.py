437de754e5494836713936204676b2a06efc1fdd
Borun Fu <fujh@cpchain.io>
2018-08-28 13:54:27 +0800

    market/database: update product table to support stream product
    Former-commit-id: b3468d02c936ad7237c8f27cf7a9530df3e02a7d

--- cpchain/market/product/serializers.py
+++ cpchain/market/product/serializers.py
@@ -10,26 +10,24 @@ class RecommendProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = (
-            'id', 'owner_address', 'title', 'description', 'tags', 'price',
-            'created', 'seq', 'file_md5', 'size',
-            'signature', 'msg_hash')
+            'id', 'owner_address', 'title', 'ptype', 'description', 'tags', 'price',
+            'created', 'seq', 'signature', 'msg_hash')
 
 
 class YouMayLikeProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = (
-            'id', 'owner_address', 'title', 'description', 'tags', 'price',
-            'created', 'seq', 'file_md5', 'size',
-            'signature', 'msg_hash')
+            'id', 'owner_address', 'title', 'ptype', 'description', 'tags', 'price',
+            'created', 'seq', 'signature', 'msg_hash')
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = (
-            'id', 'owner_address', 'title', 'description', 'tags', 'price',
-            'created', 'start_date', 'end_date', 'seq', 'file_md5', 'size',
+            'id', 'owner_address', 'title', 'ptype', 'description', 'tags', 'price',
+            'created', 'start_date', 'end_date', 'seq',
             'signature', 'msg_hash', 'status')
 
     def create(self, validated_data):
@@ -37,6 +35,7 @@ class ProductSerializer(serializers.ModelSerializer):
         product = Product(
             owner_address=validated_data['owner_address'],
             title=validated_data['title'],
+            ptype=validated_data['ptype'],
             description=validated_data['description'],
             price=validated_data['price'],
             created=now,
@@ -44,8 +43,6 @@ class ProductSerializer(serializers.ModelSerializer):
             end_date=validated_data['end_date'],
             signature=validated_data['signature'],
             owner=validated_data['owner'],
-            file_md5=validated_data['file_md5'],
-            size=validated_data['size'],
             seq=validated_data['seq'],
             msg_hash=validated_data['msg_hash'],
             tags=validated_data['tags'],
@@ -91,4 +88,3 @@ class MySellerSerializer(serializers.ModelSerializer):
     class Meta:
         model = MySeller
         fields = ('seller_public_key',)
-
