2c4234dcaddde0c8070d3701a37cdfec55897dc0
hyiwr <huangyan@cpchain.io>
2018-08-30 14:52:13 +0800

    fixed bugs of crash when the size field in the buyer table is null
    Former-commit-id: 1a35a30261b948d6a3be2f335d8b5e688fa47425

--- cpchain/utils.py
+++ cpchain/utils.py
@@ -87,6 +87,8 @@ def join_with_rc(path):
 
 def sizeof_fmt(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if num is None:
+            return 0
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
