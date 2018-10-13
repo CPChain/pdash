3ceecf13362bb7056b0a341bdf1c6c374f3bad2b
zgljl2012 <2693491512@qq.com>
2018-10-13 15:07:22 +0800

    PDAS-98: upgrade router
    Former-commit-id: a5bd69f15c064546cad3864b25eee95f48ff8b26

--- cpchain/wallet/ui.py
+++ cpchain/wallet/ui.py
@@ -21,14 +21,7 @@ from cpchain.crypto import ECCipher
 
 from cpchain.wallet.pages import load_stylesheet, wallet, main_wnd, app
 from cpchain.wallet.pages.header import Header
-from cpchain.wallet.pages.my_data import MyDataTab
-from cpchain.wallet.pages.publish import PublishProduct
-from cpchain.wallet.pages.market import MarketPage
-from cpchain.wallet.pages.detail import ProductDetail
-from cpchain.wallet.pages.purchased import PurchasedPage
-
 from cpchain.wallet.pages.login import LoginWindow
-from cpchain.wallet.pages.wallet_page import WalletPage
 
 
 # widgets
@@ -40,69 +33,11 @@ from cpchain.account import Account, get_balance
 from cpchain.wallet import utils
 from cpchain.chain.utils import default_w3 as web3
 
+from cpchain.wallet.router import Router
+
 globalLogBeginner.beginLoggingTo([textFileLogObserver(sys.stdout)])
 logger = logging.getLogger(__name__)
 
-class Router:
-
-    index = WalletPage
-    back_stack = [('market_page', [], {})]
-    forward_stack = []
-    listener = []
-
-    page = {
-        'wallet': WalletPage,
-        'market_page': MarketPage,
-        'my_data_tab': MyDataTab,
-        'publish_product': PublishProduct,
-        'product_detail': ProductDetail,
-        'purchased_page': PurchasedPage
-    }
-
-    @staticmethod
-    def addListener(listener):
-        Router.listener.append(listener)
-
-    @staticmethod
-    def removeListener(listener):
-        Router.listener.remove(listener)
-
-    @staticmethod
-    def _redirectTo(page, *args, **kwargs):
-        _page = Router.page[page](app.main_wnd.body, *args, **kwargs)
-        layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(_page)
-        QWidget().setLayout(app.main_wnd.body.layout())
-        app.main_wnd.body.setLayout(layout)
-
-    @staticmethod
-    def redirectTo(page, *args, **kwargs):
-        Router.forward_stack = []
-        Router.back_stack.append((page, args, kwargs))
-        for l in Router.listener:
-            l(page)
-        Router._redirectTo(page, *args, **kwargs)
-
-    @staticmethod
-    def hasBack():
-        return len(Router.back_stack) > 1
-
-    @staticmethod
-    def back():
-        if len(Router.back_stack) > 1:
-            Router.forward_stack.append(Router.back_stack[-1])
-            Router.back_stack = Router.back_stack[:-1]
-            page, args, kwargs = Router.back_stack[-1]
-            Router._redirectTo(page, *args, **kwargs)
-
-    @staticmethod
-    def forward():
-        if len(Router.forward_stack) > 0:
-            page, args, kwargs = Router.forward_stack[-1]
-            Router.back_stack.append((page, args, kwargs))
-            Router.forward_stack = Router.forward_stack[:-1]
-            Router._redirectTo(page, *args, **kwargs)
 
 sidebarMenu = [
     {
@@ -238,6 +173,7 @@ def __login(account=None):
     wallet.market_client.login(app.username).addCallbacks(lambda _: event.emit(events.LOGIN_COMPLETED))
     wallet.init()
     initialize_system()
+    app.router.redirectTo('market_page')
 
 def enterPDash(account=None):
     if app.main_wnd:
